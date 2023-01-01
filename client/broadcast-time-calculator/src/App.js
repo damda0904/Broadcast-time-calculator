24e8842f8f064cc48cb2e65427c05c0dc19f627f
damda0904 <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>
2023-01-01 23:03:01 +0900

    타이머 기능 추가

--- client/broadcast-time-calculator/src/App.js
+++ client/broadcast-time-calculator/src/App.js
@@ -1,9 +1,14 @@
+import { useEffect, useState, useRef } from 'react';
 import { Collapse, AutoComplete, Input } from 'antd';
 
 import './Layout.css';
 import './Element.css';
 
 const { Panel } = Collapse;
+
+const padNumber = (num, length) => {
+  return String(num).padStart(length, '0');
+};
 
 function Music({ name, time }) {
   return(
@@ -37,7 +42,39 @@ const music_list = [
   }
 ]
 
-function App() {
+function App(props) {
+  // 아무것도 입력하지 않으면 undefined가 들어오기 때문에 유효성 검사부터..
+  const tempHour = props.hour ? parseInt(props.hour) : 0;
+  const tempMin = props.min ? parseInt(props.min) : 0;
+  const tempSec = props.sec ? parseInt(props.sec) : 0;
+  // 타이머를 초단위로 변환한 initialTime과 setInterval을 저장할 interval ref
+  const initialTime = useRef(tempHour * 60 * 60 + tempMin * 60 + tempSec);
+
+  const [count_button, setCountButton] = useState("시작하기")
+  const [minute, setMin] = useState(padNumber(tempMin, 2));
+  const [second, setSec] = useState(padNumber(tempSec, 2));
+  const interval = useRef(null);
+
+  const start_test = () => {
+    if (count_button == "시작하기"){
+      setCountButton("중지")
+    } else {
+      setCountButton("시작하기")
+    }
+  }
+
+  useEffect(() => {
+    console.log("useEffect--------------------")
+    if(count_button == "중지") {
+      interval.current = setInterval(() => {
+        initialTime.current += 1;
+        setSec(padNumber(initialTime.current % 60, 2));
+        setMin(padNumber(parseInt(initialTime.current / 60), 2));
+      }, 1000);
+      return () => clearInterval(interval.current);
+    }
+  }, [count_button, second]);
+
   return (
     <div style={{background: "lightblue"}}>
       <div className='Main'>
@@ -49,7 +86,8 @@ function App() {
             <Collapse>
               <Panel header="샘플로 발표속도 측정하기">
                 <p>안녕하세요 이것은 테스트입니다. 반갑습니다 여러분</p>
-                <button>시작하기</button>
+                <p>{minute} : {second}</p>
+                <button onClick={start_test}>{count_button}</button>
               </Panel>
             </Collapse>
 
